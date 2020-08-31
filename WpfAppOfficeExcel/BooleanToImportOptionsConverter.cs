4dc0fcb9f6276200420f1b4550e0a45dd64265df
BUCKDI <BUCKDI@NB-DEHHWO-00011>
2020-08-31 08:54:01 +0200

    Projektdateien hinzufügen.

---
+++ WpfAppOfficeExcel/BooleanToImportOptionsConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace WpfAppOfficeExcel.Converter
+{
+    [ValueConversion(typeof(bool), typeof(ImportOptions.enumImportOptions))]
+    public class BooleanToImportOptionsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch (value)
+            {
+                case true:
+                    return true;
+                case false:
+                    return false;
+                default:
+                    return ImportOptions.enumImportOptions.None;
+            }
+            return ImportOptions.enumImportOptions.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value == true)
+                    return "yes";
+                else
+                    return "no";
+            }
+            return "no";
+        }
+    }
+}
---
+++ WpfAppOfficeExcel/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppOfficeExcel
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ImportOptions Import;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Import = new ImportOptions();
+
+            this.DataContext = this;
+        }
+
+        private void ButtonFileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                DefaultExt = ".CSV",
+                Title = "Eine Datei für den Import auswählen",
+                CheckFileExists = true,
+                Filter = "Csv files (*.csv)|*.csv|Alle Dateien (*.*)|*.*",
+                FilterIndex = 0
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                gbSelectOptionForImport.IsEnabled = true;
+                tbFilePathInfo.Text = openFileDialog.FileName;
+            }
+        }
+    }
+
+    public class ImportOptions : INotifyPropertyChanged
+    {
+        [Flags]
+        public enum enumImportOptions
+        {
+            None,
+            WarenEingang,
+            WarenAusgang,
+            ProduktVerlauf,
+            ProduktRetoure,
+            WarenbewegungPositiv,
+            WarenbewegungNegativ,
+            UmlagerungEingang,
+            UmlagerungAusgang,
+            Inventur
+        }
+
+        private enumImportOptions activeImportOptions;
+
+        public enumImportOptions ActiveImportOptions
+        {
+            get { return activeImportOptions; }
+            set 
+            {
+                if (activeImportOptions != enumImportOptions.None)
+                {
+                    return;
+                    //activeImportOptions = value;
+                }
+                else
+                {
+                    activeImportOptions |= value;
+                }                
+            }
+        }
+
+        public ImportOptions()
+        {
+            ActiveImportOptions = enumImportOptions.None;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
