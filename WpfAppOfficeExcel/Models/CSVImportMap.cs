1cef8cdaefcf273707489b87d1d361f2a401938b
BUCKDI <BUCKDI@NB-DEHHWO-00011>
2020-09-01 11:28:54 +0200

    Add: CSVHelper Map für Mapping CSV->Class

---
+++ WpfAppOfficeExcel/Models/CSVImportMap.cs
@@ -0,0 +1,54 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppOfficeExcel.Models
+{
+    class CSVImportMap : ClassMap<CSVImportModel>
+    {
+        public CSVImportMap()
+        {
+            Map(m => m.Typ);
+            Map(m => m.Mandant);
+            Map(m => m.FormArt);
+            Map(m => m.FormIntern);
+            Map(m => m.AufNr);
+            Map(m => m.IntPos);
+            Map(m => m.UntPos);
+            Map(m => m.LagerKey);
+            Map(m => m.AnLager);
+            Map(m => m.ArtikelNr);
+
+            Map(m => m.SerienNr);
+            Map(m => m.Kategorie);
+            Map(m => m.PosKat);
+            Map(m => m.BelegDatum);
+            Map(m => m.BelegZeit);
+            Map(m => m.Jahr);
+            Map(m => m.Periode);
+            Map(m => m.BuchungText);
+            Map(m => m.Bemerkung);
+            Map(m => m.Benutzer);
+
+            Map(m => m.Menge); 
+            Map(m => m.Kontonummer);
+            Map(m => m.Kasse);
+            Map(m => m.Bon);
+            Map(m => m.BonPosition);
+            Map(m => m.EingabeArtikelNr);
+            Map(m => m.EingabeMenge);
+            Map(m => m.Einheitspreis);
+            Map(m => m.RS-Grund);
+            Map(m => m.Lieferant);
+
+            Map(m => m.LieferDatum);
+            Map(m => m.LieferReferenz);
+            Map(m => m.Buchung);
+            Map(m => m.KontrolliertAm);
+            Map(m => m.KontrolliertDurch);
+        }
+    }
+}
