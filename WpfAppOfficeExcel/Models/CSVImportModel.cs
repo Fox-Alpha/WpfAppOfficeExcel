94dcaa839283324270bfe4f49e8622a97c4007d1
BUCKDI <BUCKDI@NB-DEHHWO-00011>
2020-09-01 11:31:09 +0200

    Fix: Klasse und Eigenschaften müssen public sein

--- WpfAppOfficeExcel/Models/CSVImportModel.cs
+++ WpfAppOfficeExcel/Models/CSVImportModel.cs
@@ -6,43 +6,43 @@ using System.Threading.Tasks;
 
 namespace WpfAppOfficeExcel.Models
 {
-    class CSVImportModel
+    public class CSVImportModel
     {
-        string Typ { get; set; }
-        int Mandant { get; set; }
-        string FormArt { get; set; }
-        int FormIntern { get; set; }
-        string AufNr { get; set; }
-        string IntPos { get; set; }
-        string UntPos { get; set; }
-        string LagerKey { get; set; }
-        string AnLager { get; set; }
-        string ArtikelNr { get; set; }
-        string SerienNr { get; set; }
-        string Kategorie { get; set; }
-        string PosKat { get; set; }
-        string BelegDatum { get; set; }
-        string BelegZeit { get; set; }
-        string Jahr { get; set; }
-        string Periode { get; set; }
-        string BuchungText { get; set; }
-        string Bemerkung { get; set; }
-        string Benutzer { get; set; }
-        string Menge { get; set; }
-        string Kontonummer { get; set; }
-        string Kasse { get; set; }
-        string Bon { get; set; }
-        string BonPosition { get; set; }
-        string EingabeArtikelNr { get; set; }
-        string EingabeMenge { get; set; }
-        string Einheitspreis { get; set; }
-        string RSGrund { get; set; } //RS-Grund
-        string Lieferant { get; set; }
-        string LieferDatum { get; set; }
-        string LieferReferenz { get; set; }
-        string Buchung { get; set; }
-        string KontrolliertAm { get; set; }
-        string KontrolliertDurch { get; set; }
+        public string Typ { get; set; }
+        public int Mandant { get; set; }
+        public string FormArt { get; set; }
+        public int FormIntern { get; set; }
+        public string AufNr { get; set; }
+        public string IntPos { get; set; }
+        public string UntPos { get; set; }
+        public string LagerKey { get; set; }
+        public string AnLager { get; set; }
+        public string ArtikelNr { get; set; }
+        public string SerienNr { get; set; }
+        public string Kategorie { get; set; }
+        public string PosKat { get; set; }
+        public string BelegDatum { get; set; }
+        public string BelegZeit { get; set; }
+        public string Jahr { get; set; }
+        public string Periode { get; set; }
+        public string BuchungText { get; set; }
+        public string Bemerkung { get; set; }
+        public string Benutzer { get; set; }
+        public string Menge { get; set; }
+        public string Kontonummer { get; set; }
+        public string Kasse { get; set; }
+        public string Bon { get; set; }
+        public string BonPosition { get; set; }
+        public string EingabeArtikelNr { get; set; }
+        public string EingabeMenge { get; set; }
+        public string Einheitspreis { get; set; }
+        public string RSGrund { get; set; } //RS-Grund
+        public string Lieferant { get; set; }
+        public string LieferDatum { get; set; }
+        public string LieferReferenz { get; set; }
+        public string Buchung { get; set; }
+        public string KontrolliertAm { get; set; }
+        public string KontrolliertDurch { get; set; }
 
         public CSVImportModel()
         {
