8d1607f276f26654715c71a82fc707bb3ad523a9
BUCKDI <BUCKDI@NB-DEHHWO-00011>
2020-09-03 17:39:27 +0200

    Chg: Background Worker in Partielle Klasse verschoben Add: Schreiben der Fehlerhaften Zeilen in ein extra Sheet "Fehlerliste"

--- WpfAppOfficeExcel/MainWindow.xaml.cs
+++ WpfAppOfficeExcel/MainWindow.xaml.cs
@@ -62,6 +62,7 @@ namespace WpfAppOfficeExcel
         private bool bEnableImportOptions = false;
 
         private BackgroundWorker worker = new BackgroundWorker();
+        private List<string[]> ErrStrLst = new List<string[]>();
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -86,9 +87,7 @@ namespace WpfAppOfficeExcel
         {
             if (this.PropertyChanged != null)
                 this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
-        }
-
-        
+        }        
 
         private void ButtonFileOpen_Click(object sender, RoutedEventArgs e)
         {
@@ -131,6 +130,9 @@ namespace WpfAppOfficeExcel
             //var map = re.ReadingContext.ReusableMemberMapData.Names[0];
             var row = re.ReadingContext.Row;
             var rec = re.ReadingContext.Record;
+            //var bld = re.ReadingContext.RawRecordBuilder.ToString();
+
+            ErrStrLst.Add(rec);
 
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("Beim Lesen der Daten aus der Import Datei ist ein Fehler aufgetreten");
@@ -157,119 +159,13 @@ namespace WpfAppOfficeExcel
             return false;
         }
 
-        void worker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture) { AllowComments = true, Delimiter = ";", HasHeaderRecord = true, TrimOptions = TrimOptions.InsideQuotes | TrimOptions.Trim, Encoding = Encoding.Default, BadDataFound=BadDataResponse, ReadingExceptionOccurred=ReadExceptionResponse };
-
-            //using (csvDataReader = new CsvDataReader(new CsvReader(new StreamReader(ImportFileName), csvConfig)))
-            //{
-            //    csvDataReader.FieldCount;
-            //    csvDataReader.get
-            //}
-
-            using (csvFileReader = new CsvReader(new StreamReader(ImportInfo.ImportFileName), csvConfig))
-            {
-                (sender as BackgroundWorker).ReportProgress(0, "Daten Import");
-                csvFileReader.Configuration.RegisterClassMap<CSVImportMap>();
-                List<CSVImportModel> recList = null;
-                try
-                {
-                    csvFileReader.Read();
-                    csvFileReader.ReadHeader();
-
-                    recList = csvFileReader.GetRecords<CSVImportModel>().ToList();
-                }
-                catch (CsvHelper.TypeConversion.TypeConverterException re)
-                {
-                }
-                
-
-                (sender as BackgroundWorker).ReportProgress(15, "Daten Extrahieren");
-
-                if (recList == null)
-                {
-                    (sender as BackgroundWorker).ReportProgress(100, "Fehler beim Daten Extrahieren");
-                    return;
-                }
-
-                //Extrahieren der Filialen
-                (sender as BackgroundWorker).ReportProgress(20, "Filialen Extrahieren");
-                var Filialen = recList.Select(l => l.LagerKey).GroupBy(x => x)
-                             .Where(g => g.Count() > 1)
-                             .Select(g => g.Key)
-                             .ToList();
-
-                Filialen.Sort();
-
-                List<List<CSVImportModel>> FilialenExport = new List<List<CSVImportModel>>();
-
-                
-                foreach (var filiale in Filialen)
-                {
-                    //ToDo: Filter auf Formular Auswahl setzen
-                    var FilOut1 = recList.Select(l => l).Where(w => w.LagerKey == filiale && w.FormArt == "WA").ToList();
-
-                    if (FilOut1.Count > 0)
-                    {
-                        FilialenExport.Add(FilOut1);
-                    }
-                }
-
-                /*
-                 * Excel Export mit ClosedXML
-                 * Datei muss existieren
-                 */
-
-                (sender as BackgroundWorker).ReportProgress(60, "Export zu Excel");
-                using (var workbook = new XLWorkbook())
-                {
-                    foreach (var item in Filialen)
-                    {
-                        var worksheet = workbook.Worksheets.Add(item);
-                        int index = Filialen.IndexOf(item);
-
-                        var rowHeader = worksheet.FirstRow();
-                        //rowHeader.Cell(1).InsertData(csvFileReader.Context.HeaderRecord);
-                        worksheet.Cell(1, 1).InsertData(csvFileReader.Context.HeaderRecord.ToList(), true);
-                        //worksheet.Cell(1, 1).AsRange();
-                        worksheet.Cell(2, 1).InsertData(FilialenExport[index]);
-                    }
-
-                    (sender as BackgroundWorker).ReportProgress(90, "Speichern der Exportdatei");
-                    workbook.SaveAs(ImportInfo.ExportFileName);
-                }
-                /*
-                 * *****************************************************************
-                 */
-
-                (sender as BackgroundWorker).ReportProgress(95, "Export abgeschlossen");
-            }
-        }
-
-        private void BadDataResponse(ReadingContext obj)
-        {
-            int row = obj.Row;
-            string col = obj.Field;
-        }
-
-        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            pbStatus.Value = e.ProgressPercentage;
-            pbStatusText.Text = e.UserState as string;
-        }
+        
 
         private void ButtonDebugFile_Click(object sender, RoutedEventArgs e)
         {
             BEnableImportOptions = true;
             
             ImportInfo.ImportFileName = @"C:\Temp\Wolsdorff\Excel_Export_Macro\Tagesbericht-WT5_1010-1015_20200801-20200810.csv";
-        }
-
-        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            pbStatus.Value = 100;
-            ButtonOpenExcelExport.IsEnabled = true;
-            BEnableImportOptions = true;
         }
 
         private void ButtonOpenExcelExport_Click(object sender, RoutedEventArgs e)
---
+++ WpfAppOfficeExcel/Models/CSVImportBackgroundWorker.cs
@@ -0,0 +1,145 @@
+﻿using ClosedXML.Excel;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAppOfficeExcel.Models;
+
+namespace WpfAppOfficeExcel
+{
+    public partial class MainWindow
+    {
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture) { AllowComments = true, Delimiter = ";", HasHeaderRecord = true, TrimOptions = TrimOptions.InsideQuotes | TrimOptions.Trim, Encoding = Encoding.Default, BadDataFound = BadDataResponse, ReadingExceptionOccurred = ReadExceptionResponse };
+
+            //using (csvDataReader = new CsvDataReader(new CsvReader(new StreamReader(ImportFileName), csvConfig)))
+            //{
+            //    csvDataReader.FieldCount;
+            //    csvDataReader.get
+            //}
+
+            using (csvFileReader = new CsvReader(new StreamReader(ImportInfo.ImportFileName), csvConfig))
+            {
+                (sender as BackgroundWorker).ReportProgress(0, "Start Daten Import");
+
+                csvFileReader.Configuration.RegisterClassMap<CSVImportMap>();
+                List<CSVImportModel> recList = null;
+                try
+                {
+                    csvFileReader.Read();
+                    csvFileReader.ReadHeader();
+
+                    recList = csvFileReader.GetRecords<CSVImportModel>().ToList();
+                }
+                catch (CsvHelper.TypeConversion.TypeConverterException re)
+                {
+                }
+
+
+                (sender as BackgroundWorker).ReportProgress(15, "Daten Extrahieren");
+
+                if (recList == null)
+                {
+                    (sender as BackgroundWorker).ReportProgress(100, "Fehler beim Daten Extrahieren");
+                    return;
+                }
+
+                //Extrahieren der Filialen
+                (sender as BackgroundWorker).ReportProgress(20, "Filialen Extrahieren und sortieren");
+
+                var Filialen = recList.Select(l => l.LagerKey).GroupBy(x => x)
+                             .Where(g => g.Count() > 1)
+                             .Select(g => g.Key)
+                             .ToList();
+
+                Filialen.Sort();
+
+                (sender as BackgroundWorker).ReportProgress(30, "Filtern der Daten nach Auswahl");
+                List<List<CSVImportModel>> FilialenExport = new List<List<CSVImportModel>>();
+
+                foreach (var filiale in Filialen)
+                {
+                    //ToDo: Filter auf Formular Auswahl setzen
+                    var FilOut1 = recList.Select(l => l).Where(w => w.LagerKey == filiale && w.FormArt == "WA").ToList();
+
+                    if (FilOut1.Count > 0)
+                    {
+                        FilialenExport.Add(FilOut1);
+                    }
+                }
+
+                /*
+                 * Excel Export mit ClosedXML
+                 * Datei muss existieren
+                 */
+
+                (sender as BackgroundWorker).ReportProgress(60, "Export zu Excel");
+
+                using (var workbook = new XLWorkbook())
+                {
+                    foreach (var item in Filialen)
+                    {
+                        var worksheet = workbook.Worksheets.Add(item);
+                        int index = Filialen.IndexOf(item);
+
+                        var rowHeader = worksheet.FirstRow();
+                        //rowHeader.Cell(1).InsertData(csvFileReader.Context.HeaderRecord);
+                        worksheet.Cell(1, 1).InsertData(csvFileReader.Context.HeaderRecord.ToList(), true);
+                        //worksheet.Cell(1, 1).AsRange();
+                        worksheet.Cell(2, 1).InsertData(FilialenExport[index]);
+                    }
+
+                    (sender as BackgroundWorker).ReportProgress(80, "Speichern Fehlerhafter Zeilen");
+
+                    if (ErrStrLst.Count > 0)
+                    {
+                        int row = 2;
+                        var worksheet = workbook.Worksheets.Add("Fehlerliste");
+                        worksheet.Cell(1, 1).InsertData(csvFileReader.Context.HeaderRecord.ToList(), true);
+
+                        foreach (var item in ErrStrLst)
+                        {
+                            worksheet.Cell(row, 1).InsertData(item.ToList(), true);
+                            row++;
+                        }
+                    }
+
+                    (sender as BackgroundWorker).ReportProgress(90, "Speichern der Exportdatei");
+                    workbook.SaveAs(ImportInfo.ExportFileName);
+                }
+                /*
+                 * *****************************************************************
+                 */
+
+                (sender as BackgroundWorker).ReportProgress(95, "Export abgeschlossen");
+            }
+        }
+
+        private void BadDataResponse(ReadingContext obj)
+        {
+            int row = obj.Row;
+            string col = obj.Field;
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbStatus.Value = e.ProgressPercentage;
+            pbStatusText.Text = e.UserState as string;
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pbStatus.Value = 100;
+            ButtonOpenExcelExport.IsEnabled = true;
+            BEnableImportOptions = true;
+        }
+    }
+}
