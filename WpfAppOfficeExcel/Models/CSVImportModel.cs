3728c894982554bfd551c1a6a9cdf307f00eaaa5
BUCKDI <BUCKDI@NB-DEHHWO-00011>
2020-09-04 14:32:51 +0200

    Todo: //Typ Umstellung ggf. Converter erstellen und verwenden ====: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht

--- WpfAppOfficeExcel/Models/CSVImportModel.cs
+++ WpfAppOfficeExcel/Models/CSVImportModel.cs
@@ -8,12 +8,17 @@ namespace WpfAppOfficeExcel.Models
 {
     public class CSVImportModel
     {
+		//Typ Umstellung ggf. Converter erstellen und verwenden
         public string Typ { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public uint Mandant { get; set; }
         public string FormArt { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public int FormIntern { get; set; }
         public string AufNr { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public int IntPos { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public int UntPos { get; set; }
         public string LagerKey { get; set; }
         public string AnLager { get; set; }
@@ -31,15 +36,20 @@ namespace WpfAppOfficeExcel.Models
         public string Menge { get; set; }
         public string Kontonummer { get; set; }
         public string Kasse { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public uint Bon { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public UInt32 BonPosition { get; set; }
         public string EingabeArtikelNr { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public int EingabeMenge { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public float Einheitspreis { get; set; }
         public string RSGrund { get; set; } //RS-Grund
         public string Lieferant { get; set; }
         public string LieferDatum { get; set; }
         public string LieferReferenz { get; set; }
+		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
         public DateTime Buchung { get; set; }
         public string KontrolliertAm { get; set; }
         public string KontrolliertDurch { get; set; }
