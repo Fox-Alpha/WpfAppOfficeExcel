4c601a4c9c5e5e3266eb624b516cbaecc68e54c0
Fox-Alpha <fox-alpha@tarnhoerner.de>
2020-09-05 00:21:55 +0200

    Chg: Datentypen vorerst auf string geändert

--- WpfAppOfficeExcel/Models/CSVImportModel.cs
+++ WpfAppOfficeExcel/Models/CSVImportModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WpfAppOfficeExcel.Models
 {
     public class CSVImportModel
@@ -11,15 +12,15 @@ namespace WpfAppOfficeExcel.Models
 		//Typ Umstellung ggf. Converter erstellen und verwenden
         public string Typ { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public uint Mandant { get; set; }
+        public string Mandant { get; set; }
         public string FormArt { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public int FormIntern { get; set; }
+        public string FormIntern { get; set; }
         public string AufNr { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public int IntPos { get; set; }
+        public string IntPos { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public int UntPos { get; set; }
+        public string UntPos { get; set; }
         public string LagerKey { get; set; }
         public string AnLager { get; set; }
         public string ArtikelNr { get; set; }
@@ -37,20 +38,20 @@ namespace WpfAppOfficeExcel.Models
         public string Kontonummer { get; set; }
         public string Kasse { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public uint Bon { get; set; }
+        public string Bon { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public UInt32 BonPosition { get; set; }
+        public string BonPosition { get; set; }
         public string EingabeArtikelNr { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public int EingabeMenge { get; set; }
+        public string EingabeMenge { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public float Einheitspreis { get; set; }
+        public string Einheitspreis { get; set; }
         public string RSGrund { get; set; } //RS-Grund
         public string Lieferant { get; set; }
         public string LieferDatum { get; set; }
         public string LieferReferenz { get; set; }
 		//TODO: Typ dynamic verwenden, falls ein Wert nicht dem erwarteten Typ  entspricht
-        public DateTime Buchung { get; set; }
+        public string Buchung { get; set; }
         public string KontrolliertAm { get; set; }
         public string KontrolliertDurch { get; set; }
 
