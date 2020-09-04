6136ccc59d7c2b24a8f9523050a8e8d9bb2bd61e
Fox-Alpha <fox-alpha@tarnhoerner.de>
2020-09-05 00:23:06 +0200

    Add: DatenConverter Klassen erstellt. ===: Es ist zur Zeit keine saubere Umwandlung in spezifische Datentypen möglich

---
+++ WpfAppOfficeExcel/Models/CSVImportConverter.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using DocumentFormat.OpenXml.Office.CustomUI;
+
+namespace WpfAppOfficeExcel.Models.Converter
+{
+    public class CSVImportCustomInt32Converter : DefaultTypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (text == "?")
+            {
+                return 0;
+            }
+
+            int c; 
+            if(int.TryParse(text, out c))
+            {
+                return c;
+            }
+
+            return null; 
+                //base.ConvertFromString(text, row, memberMapData);
+        }
+    }
+
+    public class CSVImportCustomFloatConverter : DefaultTypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            float f;
+
+            if (text == "?")
+            {
+                return 0.0f;
+            }
+
+            if (float.TryParse(text, out f))
+            {
+                return f;
+            }
+
+            return null; 
+            //base.ConvertFromString(text, row, memberMapData);    
+        }
+    }
+
+    public class CSVImportCustomDateTimeConverter : DefaultTypeConverter
+    {
+        //private string CustomDateFormat = @"yyyy-MM-dd'T'hh:mm:ss'.'FFF";
+
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            DateTime newDate = default;
+
+            if (text == "?" || string.IsNullOrEmpty(text))
+            {
+                //2019-04-01T20:08:13.929
+                //memberMapData.TypeConverterOptions.CultureInfo = CultureInfo.GetCultureInfo("de-DE");
+                //memberMapData.TypeConverterOptions.DateTimeStyle = DateTimeStyles.AssumeLocal;
+                //memberMapData.TypeConverterOptions.Formats = new string[] { "dd.MM.yyyy hh:mm:ss"};
+                //memberMapData.TypeConverterOptions.
+                
+                
+                //DateTime dt = new DateTime(1970,12,01);
+                //dt.Kind = DateTimeKind.Local;
+                //"01.01.1970 00:00:00"
+                //newDate = DateTime.ParseExact("01.01.1970 12:00:00", @"dd.MM.yyyy hh:mm:ss",  CultureInfo.InvariantCulture);
+                return null; // newDate; // base.ConvertFromString(newDate.ToString(), row, memberMapData); 
+            }
+
+            try
+            {
+                newDate = DateTime.Parse(text, CultureInfo.InvariantCulture);
+                //GetCultureInfo("de-DE")CurrentCulture.DateTimeFormat
+                //newDate = DateTime.ParseExact(text, CustomDateFormat, CultureInfo.InvariantCulture); //); //, CultureInfo.InvariantCulture, DateTimeStyles.None); //, );
+                //newDate.
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(String.Format(@"Error parsing date '{0}': {1}", text, ex.Message));
+            }
+
+            return newDate;
+
+            //return base.ConvertFromString(text, row, memberMapData);    
+        }
+    }
+
+    public class CSVImportInvalidInt32Converter : ITypeConverter
+    {
+        public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CSVImportInvalidDateConverter : ITypeConverter
+    {
+        public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CSVImportInvalidFloatConverter : ITypeConverter
+    {
+        public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CSVImportInvalidDateTimeConverter : ITypeConverter
+    {
+        public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfAppOfficeExcel/Models/CSVImportMap.cs
+++ WpfAppOfficeExcel/Models/CSVImportMap.cs
@@ -1,9 +1,13 @@
 ﻿using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfAppOfficeExcel.Models.Converter;
+
 
 namespace WpfAppOfficeExcel.Models
 {
@@ -12,12 +16,12 @@ namespace WpfAppOfficeExcel.Models
         public CSVImportMap()
         {
             Map(m => m.Typ);
-            Map(m => m.Mandant);
+            Map(m => m.Mandant);//.TypeConverter<CSVImportCustomInt32Converter>();
             Map(m => m.FormArt);
-            Map(m => m.FormIntern);
+            Map(m => m.FormIntern);//.TypeConverter<CSVImportCustomInt32Converter>();
             Map(m => m.AufNr);
-            Map(m => m.IntPos);
-            Map(m => m.UntPos);
+            Map(m => m.IntPos);//.TypeConverter<CSVImportCustomInt32Converter>();
+            Map(m => m.UntPos);//.TypeConverter<CSVImportCustomInt32Converter>();
             Map(m => m.LagerKey);
             Map(m => m.AnLager);
             Map(m => m.ArtikelNr);
@@ -36,18 +40,18 @@ namespace WpfAppOfficeExcel.Models
             Map(m => m.Menge); 
             Map(m => m.Kontonummer);
             Map(m => m.Kasse);
-            Map(m => m.Bon);
-            Map(m => m.BonPosition);
+            Map(m => m.Bon);//.TypeConverter<CSVImportCustomInt32Converter>();
+            Map(m => m.BonPosition);//.TypeConverter<CSVImportCustomInt32Converter>();
             Map(m => m.EingabeArtikelNr);
-            Map(m => m.EingabeMenge);
-            Map(m => m.Einheitspreis);
+            Map(m => m.EingabeMenge); //.TypeConverter<CSVImportCustomInt32Converter>();
+            Map(m => m.Einheitspreis);//.TypeConverter<CSVImportCustomFloatConverter>();
             Map(m => m.RSGrund).Name("RS-Grund", "RSGrund");
             Map(m => m.Lieferant);
 
             Map(m => m.LieferDatum);
             Map(m => m.LieferReferenz);
-            Map(m => m.Buchung);
-            Map(m => m.KontrolliertAm);
+            Map(m => m.Buchung); //.TypeConverter<CSVImportCustomDateTimeConverter>(); //.TypeConverterOption.NullValues("NULL", "NIL", "?", "").TypeConverterOption.DateTimeStyles( DateTimeStyles.None).TypeConverterOption.CultureInfo(CultureInfo.GetCultureInfo("de-DE"));
+            Map(m => m.KontrolliertAm); //.TypeConverter<CSVImportCustomDateTimeConverter>();//.TypeConverterOption.NullValues("NULL", "NIL", "?", "").TypeConverterOption.DateTimeStyles(DateTimeStyles.None).TypeConverterOption.CultureInfo(CultureInfo.GetCultureInfo("de-DE"));
             Map(m => m.KontrolliertDurch);
         }
     }
